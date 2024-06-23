9fe7713e69e4e25de1abd825bb3d03018ebb1aed
Paulo Henrique <paulohenriqueribeiroalves@gmail.com>
2024-06-23 12:05:46 -0300

    Criei o componente para a página não encontrada, criei os botões para a excluir e editar os eventos da lista e criei os eventos dos botões da lista de eventos.

--- src/app/components/Admin/AdminPAge.tsx
+++ src/app/components/Admin/AdminPAge.tsx
@@ -2,6 +2,9 @@
 import { useEffect, useState } from 'react';
 import * as api from '../../api/admin';
 import { Event } from '../../types/Event';
+import { EventItem, EventItemNotFounde, EventItemPlaceholder } from './events/EventItem';
+import { ItemButton } from './ItemButton';
+import { FaPlus } from 'react-icons/fa';
 
 export const AdminPage = () => {
     const [events, setEvents] = useState<Event[]>([]);
@@ -23,16 +26,29 @@ export const AdminPage = () => {
         <div>
             <div className="p-3 flex items-center">
                 <h1 className="text-2xl flex-1">Eventos</h1>
-                <div>...</div>
+                <ItemButton
+                    IconElement={FaPlus}
+                    onClick={() => { }}
+                />
             </div>
             <div className="my-3">
                 {!loading && events.length > 0 && events.map(item => (
-                    <div key={item.id}>{item.title}</div>
+                    <EventItem
+                        key={item.id}
+                        item={item}
+                        refreshAction={loadEvents}
+                        openModal={() => {}}
+                    />
                 ))}
                 {!loading && events.length === 0 &&
-                    <div>Nada Encontrado</div>
+                    <EventItemNotFounde/>
                 }
-                {loading && <div>Carregando...</div>}
+                {loading && 
+                    <>
+                        <EventItemPlaceholder/>
+                        <EventItemPlaceholder/>
+                    </>
+                }
             </div>
         </div>
     );
